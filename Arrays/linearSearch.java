b0ea4894ef074a6bee3dd50d2487bcc7ed3e5db7
Vivek Shukla <vivekshukla3011@gmail.com>
2024-02-17 00:18:37 +0530

    Linear Search

--- Arrays/linearSearch.java
+++ Arrays/linearSearch.java
@@ -1,5 +1,23 @@
 package Arrays;
 
+// Write the code for linear search.
+
 public class linearSearch {
     
+    public static int linSearch(int arr[], int key){
+        
+        for(int i=0; i<arr.length; i++){
+            if(arr[i] == key){
+                return i;
+            }
+        }
+        return -1;
+    }
+    public static void main(String[] args) {
+        
+        int arr[] = {8,10,4,1,15,9};
+        int key = 15;
+        
+        System.out.println(linSearch(arr, key));
+    }
 }
